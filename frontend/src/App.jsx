d5a893017100a744e4867915e3b4f0fb8af473ce
Jane Batakariza <janebatakariza@gmail.com>
2026-03-31 14:55:40 +0200

    fetching data on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,19 @@
-import axios from 'axios';
-import { useEffect,useState } from 'react';
-import './App.css'
+import axios from "axios";
+function App() {
+  const fetchData = async () => {
+    try {
+      await axios.get("http://localhost:3000/students");
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-
-function App(){
-    const [users, setUsers] = useState([]);
-    useEffect(()=>{
-      try {
-        const fetchUsers = async()=>{
-          const res = await axios.get("http://localhost:3000/api/admin");
-          if(res.status ===200){
-              setUsers(res.data.users)
-          
-          }
-        }
-        fetchUsers()
-
-        
-      } catch (error) {
-        console.log(error)
-      }
-    },[])
-    console.log(users)
-
+  const fetchedData = fetchData();
   return (
     <>
-    <div>
-      <div>
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>Email</th>
-            <th>Role</th>
-          </tr>
-        </table>
-      {
-        users.map(user =>(
-          <div className='div'>
-            <p>{user.fname}</p>
-            <p>{user.phone}</p>
-            <p>{user.email}</p>
-            <p>{user.role}</p>
-          </div>
-        ))
-      }
-      </div>
-    </div>
+      <p>{fetchedData}</p>
+      <p>Think its working</p>
     </>
   )
 }
-
 export default App;
